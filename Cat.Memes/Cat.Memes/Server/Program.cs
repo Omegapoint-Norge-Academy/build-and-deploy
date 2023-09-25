832cdb5e672d610a5d04018629b35867ebffc1c2
Eivind Jahr Kirkeby <ejk@itverket.no>
2023-09-25 19:37:12 +0200

    fix config

--- Cat.Memes/Cat.Memes/Server/Program.cs
+++ Cat.Memes/Cat.Memes/Server/Program.cs
@@ -17,8 +17,8 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddHttpClient<ICatMemeService, CatMemeService>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["CatMemeApi:Url"] ?? throw new InvalidOperationException());
-    var authenticationString = $"{builder.Configuration["CatMemeApi:Username"]}:{builder.Configuration["CatMemeApi:Password"]}";
+    client.BaseAddress = new Uri(builder.Configuration["ApiUrl"] ?? throw new InvalidOperationException());
+    var authenticationString = $"{builder.Configuration["ApiUser"]}:{builder.Configuration["ApiPassword"]}";
     var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(authenticationString));
     client.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
 });
